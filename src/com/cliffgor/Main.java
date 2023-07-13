36b462fedaa02e1be87a522d4a362293d573d6a6
cliffgor <clierozigy@gmail.com>
2023-07-13 15:20:15 +0300

    First things to learn about Java

---
+++ src/com/cliffgor/Main.java
@@ -0,0 +1,13 @@
+package com.cliffgor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!😋");
+        System.out.println(10 + 10 );
+//        If it's not a method nor a class it ends with a semicolon ;
+//        Everything within the class is the class body
+//        System.out.println - Allows someone to pass anything inside it to be printed in the console
+//        The shortcut - SOUT (GIVES OUT System.out.println)
+//        public, static, class, void  are reserved keywords in java
+    }
+}
