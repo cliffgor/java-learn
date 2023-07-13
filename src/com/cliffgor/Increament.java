51b92a7be46aae5b4e9f4b76853c2695d9909a8e
cliffgor <clierozigy@gmail.com>
2023-07-13 17:36:37 +0300

    Learnt about decrement and increment of numbers

---
+++ src/com/cliffgor/Increament.java
@@ -0,0 +1,13 @@
+package com.cliffgor;
+
+public class Increament {
+
+    public static void main(String[] args) {
+        int number = 0;
+        int increment = number + 1;
+        int decrement = number - 1;
+        System.out.println(increment);
+        System.out.println(decrement);
+
+    }
+}
