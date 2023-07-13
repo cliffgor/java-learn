9437b2388d7a36309e3d9b22c7cb66f8c4db23cc
cliffgor <clierozigy@gmail.com>
2023-07-13 16:18:03 +0300

    Learnt about operators and how they work

---
+++ src/com/cliffgor/Operators.java
@@ -0,0 +1,14 @@
+package com.cliffgor;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+    }
+}
