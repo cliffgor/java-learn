1a14d48b3df3ca6efc7d81b6ea71b2d8fa95e058
cliffgor <clierozigy@gmail.com>
2023-07-13 16:08:57 +0300

    Learnt about data types and what they do

--- src/com/cliffgor/Main.java
+++ src/com/cliffgor/Main.java
@@ -2,6 +2,19 @@ package com.cliffgor;
 
 public class Main {
     public static void main(String[] args) {
+//        Primitives - Allows us to store simple values
+//        int - datatype that allows one to store whole numbers
+//        double - datatype that allows one to store decimal numbers
+//        boolean -  stores condition
+//        char - stores a letter
+        int number1 = 20;
+        int number2 = 20;
+        int result = number1 + number2;
+        System.out.println(result);
+        double pi = 3.14;
+        boolean isAdult = true;
+        char a = 'A';
+
         System.out.println("Hello world!😋");
         System.out.println(10 + 10 );
 //        If it's not a method nor a class it ends with a semicolon ;
@@ -9,5 +22,9 @@ public class Main {
 //        System.out.println - Allows someone to pass anything inside it to be printed in the console
 //        The shortcut - SOUT (GIVES OUT System.out.println)
 //        public, static, class, void  are reserved keywords in java
+        /*
+        Variables - Allows one to store a value into a placeholder which
+        you can use later i.e storing a number
+         */
     }
 }
